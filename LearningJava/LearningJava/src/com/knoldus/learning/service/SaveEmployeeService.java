d52a80024c70d47916e5eb5a533ffd077a4191ac
AnuradhaKumari201 <anu@codewithanu.com>
2021-03-09 00:57:24 +0530

    Anuradha's assignment

--- LearningJava/LearningJava/src/com/knoldus/learning/service/SaveEmployeeService.java
+++
@@ -1,9 +0,0 @@
-package com.knoldus.learning.service;
-
-import com.knoldus.learning.entity.Employee;
-
-public interface SaveEmployeeService {
-    void saveEmployee(Employee employee);
-    int getEmployeeCount();
-
-}
